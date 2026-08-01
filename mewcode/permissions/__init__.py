44fe756a3ce20b6763aabc282e026a8ed6304f6c
朱福培 <2163431938@qq.com>
2026-08-01 10:34:12 +0800

    update code

--- mewcode/permissions/__init__.py
+++ mewcode/permissions/__init__.py
@@ -1,9 +1,3 @@
-# 来源：公众号@小林coding
-# 后端八股网站：xiaolincoding.com
-# Agent网站：xiaolinnote.com
-# 简历模版：jianli.xiaolinnote.com
-
-
 from mewcode.permissions.checker import Decision, PermissionChecker
 from mewcode.permissions.dangerous import DangerousCommandDetector
 from mewcode.permissions.modes import DecisionEffect, PermissionMode, mode_decide
